70f4045f945ca55a3d0958a148be171d45d894a0
Torregu <torregu99@gmail.com>
2022-09-23 01:51:09 +0200

    Add new files in 111. Operations with other types

---
+++ Courses/Programming/Python/Introduction to Python/1 Python Basics/11. Operations with other types/Exercise.md
@@ -0,0 +1,7 @@
+**Operations with other types**
+
+Hugo mentioned that different types behave differently in Python.
+
+When you sum two strings, for example, you'll get different behavior than when you sum two integers or two booleans.
+
+In the script some variables with different types have already been created. It's up to you to use them.
---
+++ Courses/Programming/Python/Introduction to Python/1 Python Basics/11. Operations with other types/script.py
@@ -0,0 +1,15 @@
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of savings and growth_multiplier to year1
+year1 = savings * growth_multiplier
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
