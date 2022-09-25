267d516a03f772f3829c756282b0d3019f98273f
Torregu <56200869+Torregu@users.noreply.github.com>
2022-09-25 23:47:18 +0200

    Update script.py

--- Courses/Programming/Python/Introduction to Python/4 NumPy/12. 2D Arithmetic/script.py
+++ Courses/Programming/Python/Introduction to Python/4 NumPy/12. 2D Arithmetic/script.py
@@ -8,8 +8,10 @@ import pandas as pd
 url_data = "https://github.com/Torregu/DataCamp/blob/main/Courses/Programming/" + \
            "Python/Introduction%20to%20Python/datasets/baseball.csv?raw=true"
 
-# + Read the csv from url_data then select the fields "Height" "Weight" "Age", convert the entries into lists and create a list of these lists
+# + Read the csv from url_data then select the fields "Height" "Weight" "Age", convert the entries into 'lists' and create a list of these lists
 baseball = pd.read_csv(url_data, sep = ",", decimal = ".", header = 0)[["Height", "Weight", "Age"]].values.tolist()
+
+# + Read the update.csv file and save it in a new variable called updated
 updated = pd.read_csv("https://github.com/Torregu/DataCamp/blob/main/Courses/Programming/" + \
 		      "Python/Introduction%20to%20Python/datasets/update.csv?raw=true", header = None).values 
 # + .to_numpy may also be used instead of .values
