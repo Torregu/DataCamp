f7ae961db3d6e7eac49d72fc8f9922a552f871c5
Torregu <torregu99@gmail.com>
2023-01-31 01:06:45 +0100

    Remove files

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cp ./TwitterSource.java ./flume_conf/flume-sources/src/main/java/com/cloudera/flume/source/
-rm -Rf ./flume_conf/flume-sources/src/main/java/com/cloudera/flume/source/.ipynb_checkpoints/
-cd ./flume_conf/flume-sources
-mvn compile
-mvn install
-cp target/flume-sources-1.0-SNAPSHOT.jar ../
-cd ../..
